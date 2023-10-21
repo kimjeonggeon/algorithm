ed291dbe520d6357b712c63a4780bdf84971b2e7
jeonggeon <kjkkmk42397@gmail.com>
2023-10-21 16:07:31 +0900

    first commit

---
+++ src/backjoon/algorithm1330.java
@@ -0,0 +1,19 @@
+package backjoon;
+import java.util.Scanner;
+
+public class algorithm1330 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		in.close();
+		
+		String str = (A>B ? ">": ((A<B)? "<" :"=="));
+		System.out.println(str);
+	}
+	
+	
+}
