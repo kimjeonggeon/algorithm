421e73ef0fecbcfd5eb6328569d2c4643173d488
jeonggeon <kjkkmk42397@gmail.com>
2023-12-27 12:47:58 +0900

    HASHMAP-1

---
+++ src/inflearn/HashTree_1.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashTree_1 {
+	public char solution(int n, String s) {
+		char answer =' ';
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(char x: s.toCharArray()) {
+		map.put(x, map.getOrDefault(x,0)+1);	
+		}
+		int max = Integer.MIN_VALUE;
+		for(char key : map.keySet()) {
+			if(map.get(key)>max) {
+				max = map.get(key);
+				answer = key;
+			}
+		}
+		return answer;
+		
+	}
+	public static void main(String[] args) {
+		HashTree_1 T = new HashTree_1();
+		Scanner kb = new Scanner(System.in); 
+		int n = kb.nextInt();
+		String str = kb.next();
+		System.out.println(T.solution(n, str));
+	
+	}
+}
